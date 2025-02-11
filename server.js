f2dcda6fc02cf5dfbe6ca505331016eb77610a38
showrav <showravdas8@gmail.com>
2025-02-11 21:30:14 +0600

    error fixed

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ connectDB();
 app.use("/auth", authRoutes);
 app.use("/tasks", taskRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
